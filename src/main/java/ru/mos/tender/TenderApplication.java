38f71e3fb0090a47875248b1d57fdf118cd8727d
voteva <tatianavoteva@gmail.com>
2020-01-25 16:40:28 +0300

    Initial commit

---
+++ src/main/java/ru/mos/tender/TenderApplication.java
@@ -0,0 +1,11 @@
+package ru.mos.tender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TenderApplication.class, args);
+    }
+}
