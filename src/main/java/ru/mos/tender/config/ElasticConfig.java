88ecf223ad09c498be9c517a3b9bd4edaacc19f1
Blagushin Rostislav <blagushin.r.f@sberbank.ru>
2020-01-25 18:48:08 +0300

    Add ElasticSearch config

---
+++ src/main/java/ru/mos/tender/config/ElasticConfig.java
@@ -0,0 +1,45 @@
+package ru.mos.tender.config;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+import java.net.InetAddress;
+
+@Configuration
+@EnableElasticsearchRepositories
+public class ElasticConfig {
+
+    @Value("${elasticsearch.host}")
+    private String esHost;
+
+    @Value("${elasticsearch.port}")
+    private int esPort;
+
+    @Value("${elasticsearch.clustername}")
+    private String esClusterName;
+
+    @Bean
+    public Client client() throws Exception {
+
+        Settings esSettings = Settings.builder()
+                .put("cluster.name", esClusterName)
+                .build();
+
+        return new PreBuiltTransportClient(esSettings)
+                .addTransportAddress(new TransportAddress(InetAddress.getByName(esHost), esPort));
+    }
+
+    @Bean
+    public ElasticsearchTemplate elasticsearchTemplate() throws Exception {
+        return new ElasticsearchTemplate(client());
+    }
+
+
+}
